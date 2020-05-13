06a6bb362a7b6ac71e0efbe40c8d010a0abe5003
Victor Soares <victor.soares@elotech.com.br>
2020-05-13 00:15:32 -0300

    adicionado mesh e animação de personagem

---
+++ Assets/Scripts/BruteWarriorAnimationController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BruteWarriorAnimationController : MonoBehaviour
+{
+
+    public SimpleTransformMove move;
+
+    public Rigidbody rb;
+
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        move.OnGroundTouch += this.OnGroundTouch;
+        move.OnJump += this.OnJump;
+        move.OnAttack += this.OnAttack;
+    }
+
+    void Update()
+    {
+        bool moving = rb.velocity.magnitude > 0;
+        animator.SetBool("Moving", moving);
+        animator.SetInteger("Velocity", (int) move.Speed);
+    }
+     void OnGroundTouch()
+    {
+        animator.SetBool("OnAir", false);
+    }
+
+     void OnAttack()
+    {
+        animator.SetTrigger("Attack");
+    }
+
+    void OnJump()
+    {
+        Debug.Log("Jump");
+        animator.SetTrigger("Jump");
+        animator.SetBool("OnAir", true);
+    }
+}
---
+++ Assets/Scripts/RotateTowardsVelocity.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateTowardsVelocity : MonoBehaviour
+{
+    public float Velocity = 1;
+    public Rigidbody rb;
+
+    private Quaternion eulerOffset;
+
+    private void Start()
+    {
+        eulerOffset = Quaternion.Euler(new Vector3(0, 0, 0));
+    }
+
+    void Update()
+    {
+        Vector3 direcao = rb.velocity.normalized;
+        if (direcao != Vector3.zero && transform.forward != direcao)
+        {
+            Quaternion rotation = Quaternion.LookRotation(direcao) * eulerOffset;
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, rotation, Velocity * Time.deltaTime);
+        }
+    }
+}
